cfce4a1b5a92ffbc616d6243740b1cfe504bcb3e
Dante2306 <116041887+Dante2306@users.noreply.github.com>
2023-01-19 09:21:59 -0300

    Add files via upload

---
+++ js/products.js
@@ -0,0 +1,34 @@
+const productos = [
+    {
+      id: 1,
+      nombre: "Gorra",
+      precio: 50,
+      img:
+        "https://http2.mlstatic.com/D_NQ_NP_924982-MLA49939105818_052022-O.jpg",
+      cantidad: 1,
+    },
+    {
+      id: 2,
+      nombre: "Chomba",
+      precio: 100,
+      img:
+        "https://http2.mlstatic.com/D_NQ_NP_932309-MLA47174484257_082021-W.jpg",
+      cantidad: 1,
+    },
+    {
+      id: 3,
+      nombre: "Buzo",
+      precio: 150,
+      img:
+        "https://http2.mlstatic.com/D_NQ_NP_619958-MLA49737096721_042022-W.jpg",
+      cantidad: 1,
+    },
+    {
+      id: 4,
+      nombre: "Mono",
+      precio: 200,
+      img:
+        "https://cdn-1.motorsport.com/images/mgl/6zQ7OKeY/s8/max-verstappen-red-bull-racing-1.jpg",
+      cantidad: 1,
+    },
+  ];
